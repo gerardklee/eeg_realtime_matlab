47de8c6c31ecb40605f20f7aa20be6ea5156dd3e
GerardProject <gerardklee@hotmail.com>
2020-01-07 14:20:33 -0800

    graph function implemented and well-corresponded to brain signals

--- src/main.m
+++ src/main.m
@@ -1,4 +1,4 @@
-function output = main()
+function main()
 
     % adding paths
     addpath(genpath('/users/gerardlee/desktop/eeg_realtime_matlab/OpenBCI_MATLAB/Matlab-Python/labstreaminglayer'));
@@ -16,9 +16,10 @@ function output = main()
     % resolve a stream...
     disp('resolving an EEG stream...');
     result = {};
+    error_timer = 15;
     error_tracker = 0;
     while isempty(result)
-        if error_tracker == 15
+        if error_tracker == error_timer
             disp('check your connection');
             break
         end
@@ -35,35 +36,47 @@ function output = main()
     
     % create a new inlet
     disp('opening an inlet...');
-    inlet = lsl_inlet(result{1});
+    inlet = lsl_inlet(result{1});  
     
-    disp('now receiving data...');
-    
-    %figure('position', [100, 100, 800, 2000]);  
-    % run to print each value of the stream
-    
+    disp('now receiving data...');   
     disp("graph begins..");
-    output = {}
-    i = 1;
-    while true
-     
-        % get data from the inlet
-        [vec, ts] = inlet.pull_sample();
-        data = vec(1, 1);
-        output{i} = data;
-        if i > 2
-            plot([i-1 i], [output{i-1};output{i}], 'b-'); hold on;
-            xlim([max([0, i - 1000]), max([1000, i])]);
-        end
-        i = i + 1;
-        disp(i);
-    end
-    
-    
-    
-    
+    get_plot(inlet);
+end
 
+function get_plot(inlet)
+    x_points = [];
+    y_points = [];
+    figure;
+    for i = 1:5000
+        disp(i);
+        [vec] = inlet.pull_sample();
+        data = vec(1, 1);
         
+        % pre-allocate the array for speed
+        x_temp = zeros(1, length(x_points) + 1);
+        y_temp = zeros(1, length(y_points) + 1);
+        
+        if length(x_temp) == 1
+            x_points = x_temp;
+            x_points(1, 1) = i;
+            y_points = y_temp;
+            y_points(1, 1) = data;
+            continue;
+        end
+        
+        x_temp = x_points;
+        y_temp = y_points;
+        x_temp(1, i - 1) = x_points(1, length(x_points));
+        y_temp(1, i - 1) = y_points(1, length(y_points));
+        x_temp(1, i) = i;
+        y_temp(1, i) = data;
+        
+        x_points = x_temp;
+        y_points = y_temp; 
+ 
+    end
+    plot(x_points, y_points);
+end
     
     
     
